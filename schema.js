97c9c50de8f6b2e4701e662f46d756ee07e61f85
Hhenry88 <henryhan88@outlook.com>
2017-08-18 21:15:26 +0800

    getPersonByURL helper function

--- schema.js
+++ schema.js
@@ -1,7 +1,43 @@
-import  { GraphQLSchema, GraphQLObjectType } from 'graphql';
+import  { GraphQLSchema, GraphQLObjectType, GraphQLList } from 'graphql';
 import fetch from 'node-fetch';
 
 const DATA_URL = 'http://localhost:8000';
+
+function getPersonByURL(relativeURL){
+  fetch(`${DATA_URL}${relativeURL}`)
+    .then(res => res.json())
+    .then(json => json.person)
+}
+
+const PersonType = new GraphQLObjectType({
+  name: "Person",
+  description: '...',
+
+  fields:() => ({
+    firstName: {
+      type: GraphQLString,
+      resolve: (person) => person.first_name
+    },
+    lastName: {
+      type: GraphQLString,
+      resolve: (person) => person.last_name
+    },
+    email: {
+      type: GraphQLString,
+    },
+    username: {
+      type: GraphQLString,
+    },
+    id: {
+      type: GraphQLString
+    },
+    friends: {
+      type: new GraphQLList(PersonType),
+      resolve: (person) => 
+        person.friends.map(getPersonByURL)
+    }
+  })
+});
 
 const QueryType = new GraphQLObjectType({
   name: 'Query',
@@ -17,9 +53,7 @@ const QueryType = new GraphQLObjectType({
       },
       resolve: (root, args) => {
         //can do promises, http requests, etc.
-        fetch(`${DATA_URL}/people/${args.id}/`)
-          .then(res => res.json())
-          .then(json => json.person)
+        return getPersonByURL(`/people/${args.id}/`);
       }
     }
   })
