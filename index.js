ec8e3750748255e55915103b514faed94699e0d7
Hhenry88 <henryhan88@outlook.com>
2017-08-18 21:24:47 +0800

    add dataloaders to index

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
+import DataLoader from 'dataloader';
 
 import schema from './schema';
 
 const app = express();
 
-app.use(graphQLHTTP({
-  //pass in a schema
-  schema,
-  //turn on graphiql
-  graphiql: true,
+app.use(graphQLHTTP( req => {
+  const personLoader = new DataLoader(
+    keys => Promise.all(key.maps(getPersonByURL))
+  )
+  const loaders = {
+    person: personLoader
+  }
+  return {
+    //pass in a schema
+    context: {loaders},
+    schema,
+    //turn on graphiql
+    graphiql: true,
+  }
 }))
 
 app.listen(4000);
